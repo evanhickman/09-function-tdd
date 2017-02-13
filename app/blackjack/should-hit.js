ad59e9102f5e814c657e61823a56d372b76e714a
Evan Hickman <travisevanhickman@gmail.com>
2017-02-13 17:06:19 -0600

    finish blackjack

--- app/blackjack/should-hit.js
+++ app/blackjack/should-hit.js
@@ -5,5 +5,13 @@ export default function (a, b) {
     return false;
   }
 
+  if (cardSum(a, b) <= 17) {
+    return true;
+  }
+
+  if (cardSum(a, b) > 17) {
+    return false;
+  }
+
   return true;
 }
