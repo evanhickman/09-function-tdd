76e3b3497b70d47d7037501cafa1f8ccd65e2295
Evan Hickman <travisevanhickman@gmail.com>
2017-02-13 20:15:17 -0600

    conway

--- app/conway/alive-neighbors.js
+++ app/conway/alive-neighbors.js
@@ -1,1 +1,11 @@
-export default function () {}
+export default function (a) {
+  const length = a.length;
+  let count = 0;
+  for (let i = 0; i < length; i += 1) {
+    if (a[i] === true) {
+      count += 1;
+    }
+  }
+
+  return count;
+}
